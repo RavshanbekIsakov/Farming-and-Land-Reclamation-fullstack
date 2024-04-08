35f00f6779b5655d5945c85ba3ee12c79bc87c5d
RavshanbekIsakov <risakov0809@gmail.com>
2024-04-09 01:17:23 +0500

    started admin panel, made admin auth

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,9 +2,71 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Admin;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AdminController extends Controller
 {
-    //
+    public function auth(Request $request){
+        $request->validate([
+            'username' => "required|string",
+            'password' => "required|string",
+        ]);
+        $admin = Admin::where('username', $request->username)->first();
+//        return Hash::make($request->password);
+        if (!empty($admin)){
+            if (Hash::check($request->password, $admin->password)){
+                session()->put('admin',1);
+                session()->put('id',$admin->id);
+                session()->put('fullname',$admin->fullname);
+                return redirect()->route('admin.profile');
+            }
+            else{
+                return back()->with("login_error",1);
+            }
+        }
+        else{
+            return back()->with("login_error",1);
+        }
+    }
+
+    public function logout(){
+        session()->flush();
+        return redirect()->route('admin.login');
+    }
+
+    public function update_password(Request $request){
+        if ($request->password1 != $request->password2){
+            return back()->with('logic_error',1);
+        }
+        else{
+            $p = Hash::make($request->password1);
+            $admin = admins::find(session('id'));
+            $admin->password = $p;
+            $admin->save();
+            return back()->with('success',1);
+        }
+    }
+
+    public function updatePhoto(Request $request)
+    {
+        $request->validate([
+            'photo' => 'required|image|mimes:jpeg,png,jpg|max:2048', // Example validation rules
+        ]);
+
+        $photo = $request->file('photo');
+
+        $filename = time() . '.' . $photo->getClientOriginalExtension();
+
+        $photo->storeAs('public/images', $filename);
+
+        $admin = Auth::guard('admin')->user();
+
+        $admin->photo = $filename;
+        $admin->save();
+
+        return redirect()->back()->with('success', 'Photo updated successfully');
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
@@ -16,3 +17,15 @@ use App\Http\Controllers\UserController;
 Route::get('/', [UserController::class, 'index'])->name('user.index');
 Route::get('/contacts', [UserController::class, 'contacts'])->name('user.contacts');
 Route::get('/references', [UserController::class, 'references'])->name('user.references');
+
+
+Route::prefix('admin')->group(callback: function () {
+    Route::view('/', 'admin.login')->name("admin.login");
+    Route::post('/auth', [AdminController::class,'auth'])->name('admin.auth');
+
+    Route::middleware(['adminAuth'])->group(function () {
+        Route::get('logout', [AdminController::class, 'logout'])->name('admin.logout');
+        Route::view('profile','admin.profile')->name('admin.profile');
+        Route::post('profile',[AdminController::class, 'update_password'])->name('admin.password.update');
+    });
+});
